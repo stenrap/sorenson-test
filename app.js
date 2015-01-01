1d59dcb15dd4c8cdf51e29f2b0af66f3195a6e75
Rob Johansen <robert_johans22@hotmail.com>
2015-01-01 08:10:30 -0700

    Commit the beginnings of the 'add' page.

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var index = require('./routes/index');
+var add   = require('./routes/add');
 
 var app = express();
 
@@ -22,8 +22,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', index);
+app.use('/add', add);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -55,6 +55,5 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
-
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.render('index', {
-    pageTitle: '✧ Polaris',
-    title: ' Polaris'
-  });
+  res.render('index', {});
 });
 
 module.exports = router;
