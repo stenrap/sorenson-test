b60c250af63bd1deeef18e8a57e1f8ec57bab92c
Rob Johansen <robert_johans22@hotmail.com>
2014-12-31 19:56:57 -0700

    Add Bootstrap and start defining the layout.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    pageTitle: '✧ Polaris',
+    title: ' Polaris'
+  });
 });
 
 module.exports = router;
